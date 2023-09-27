5255b37a0cbe51b2b90432225266566880c6f3de
laqudee <24524492+laqudee@users.noreply.github.com>
2023-09-27 14:46:35 +0800

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# C Project Learn And Action
+
+## 执行方式
+
+```sh
+gcc -std=c11 [filename].c
+```
---
+++ chapter-2/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    num = 1;
+    printf("I am a simple");
+    printf("program.\n");
+    printf("My favorite number is %d.\n", num);
+    return 0;
+}
