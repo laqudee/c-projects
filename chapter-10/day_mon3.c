1619ac59c223d989893a4fcf7ef15c10ac2a6992
laqudee <24524492+laqudee@users.noreply.github.com>
2023-10-23 15:37:01 +0800

    feat chapter 10 code & note

--- chapter-10/day_mon3.c
+++ chapter-10/day_mon3.c
@@ -1,11 +1,14 @@
+/**
+ * use pointer notaation
+ */
+
 #include <stdio.h>
 #define MONTHS 12
 
 int main(void)
 {
     int days[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int index;
-    for (index = 0; index < MONTHS; index++)
-        printf("Month %2d has %2d days.\n", index + 1, days[index]);
+    for (int i = 0; i < MONTHS; i++)
+        printf("Month %2d has %d days.\n", i + 1, *(days + i));
     return 0;
 }
