5c6628fdac2ccb22eb671f297928adcdcd2a8f14
laqudee <24524492+laqudee@users.noreply.github.com>
2023-10-26 16:48:07 +0800

    feat chapter 12 code and note

--- chapter-12/rand_num/r_drive1.c
+++ chapter-12/rand_num/r_drive1.c
@@ -1,6 +1,7 @@
 // 与s_and_r.c一起编译
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 extern void srand1(unsigned int x);
 extern int rand1(void);
@@ -13,6 +14,7 @@ int main(void)
     while (scanf("%u", &seed) == 1)
     {
         srand1(seed); // 重置种子
+        // srand1((unsigned int)time(0));
         for (count = 0; count < 5; count++)
             printf("rand1() = %d\n", rand1());
         printf("Please enter next seed(q to quit).\n");
