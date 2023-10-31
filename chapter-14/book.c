43ec8cb647ad718b3dab597f69da28a0eebd0c03
laqudee <24524492+laqudee@users.noreply.github.com>
2023-10-31 16:45:41 +0800

    feat chapter-14 code and note

---
+++ chapter-14/book.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "util.h"
+#define MAXTITL 41
+#define MAXAUTL 31
+
+struct book
+{
+    char title[MAXTITL];
+    char author[MAXAUTL];
+    float value;
+};
+
+int main(void)
+{
+    struct book library;
+    printf("Please enter the book title.\n");
+    s_gets(library.title, MAXTITL);
+    printf("Now enter the author.\n");
+    s_gets(library.author, MAXAUTL);
+    printf("Now enter the value.\n");
+    scanf("%f", &library.value);
+    printf("%s by %s: $%.2f\n", library.title,
+           library.author, library.value);
+    printf("%s: \"%s\" ($%.2f)\n", library.author,
+           library.title, library.value);
+    printf("Done.\n");
+
+    return 0;
+}
