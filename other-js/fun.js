9f792be2c0e6c81cd2208305dc51768dbdcd1e9a
laqudee <24524492+laqudee@users.noreply.github.com>
2023-11-02 15:50:30 +0800

    Update fun.js

--- other-js/fun.js
+++ other-js/fun.js
@@ -5,21 +5,59 @@ function delay(ms) {
 }
 
 function showCircle(x, y, radius) {
-    let div = document.createElement("div");
-    div.style.width = 0;
-    div.style.height = 0;
-    div.style.left = cx + "px";
-    div.style.top = cy + "px";
-    div.className = "circle";
-    document.body.appendChild(div);
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            div.style.width = radius * 2 + "px";
-            div.style.height = radius * 2 + "px";
-            div.addEventListener('transitionend', function handler() {
-                div.removeEventListener('transitionend', handler);
-                resolve(div);
-            })
-        }, 0)
-    })
+  let div = document.createElement("div");
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + "px";
+  div.style.top = cy + "px";
+  div.className = "circle";
+  document.body.appendChild(div);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + "px";
+      div.style.height = radius * 2 + "px";
+      div.addEventListener("transitionend", function handler() {
+        div.removeEventListener("transitionend", handler);
+        resolve(div);
+      });
+    }, 0);
+  });
+}
+
+let urls = [
+  "https://api.github.com/users/iliakan",
+  "https://api.github.com/users/remy",
+  "https://api.github.com/users/jeresig",
+];
+
+let requests = urls.map((url) => fetch(url));
+
+Promise.all(requests).then((responses) => {
+  responses.forEach((response) => {
+    console.log(response.url, response.status);
+  });
+});
+
+Promise.allSettled(urls.map((url) => fetch(url))).then((results) => {
+  results.forEach((result, num) => {
+    if (result.status == "fulfilled") {
+      alert(`${urls[num]}: ${result.value.status}`);
+    }
+    if (result.status == "rejected") {
+      alert(`${urls[num]}: ${result.reason}`);
+    }
+  });
+});
+
+if (!Promise.allSettled) {
+  const rejectHandler = (reason) => ({ status: "rejected", reason });
+
+  const resolveHandler = (value) => ({ status: "fulfilled", value });
+
+  Promise.allSettled = function (promises) {
+    const convertedPromises = promises.map((p) =>
+      Promise.resolve(p).then(resolveHandler, rejectHandler)
+    );
+    return Promise.all(convertedPromises);
+  };
 }
